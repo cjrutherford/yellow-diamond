6b9551ad45dea8aafb0fff2955bb9c0263e94b7b
Chris Rutherford <chris@dittach.com>
2019-03-07 12:38:23 -0500

    working on the header component, and adding the router components to allow for different pages to be displayed.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import Login from "./components/login";
 import Register from "./components/register";
 import ResetUser from "./components/reset";
+import Header from "./components/header";
 import "./App.css";
 
 class App extends Component {
 	render() {
-		return <Register />;
+		return <Header />;
 	}
 }
 
---
+++ client/src/components/header.js
@@ -0,0 +1,51 @@
+import React, { Component, Fragment } from "react";
+import YellowDiamond from "../assets/Yellow Diamond.gif";
+class Header extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			headerBG: {
+				backgroundImage: 'url("blob.gif")',
+				width: "100vw",
+				height: "13vh",
+				backgroundPosition: "center center",
+				backgroundSize: "cover",
+				margin: "0",
+				padding: "0"
+			},
+			headerFilter: {
+				background: "rgba(100, 100, 100, 0.8)",
+				width: "98vw",
+				height: "10vh",
+				display: "flex",
+				flexDirection: "row",
+				alignItems: "center",
+				justifyContent: "space-between",
+				margin: "0",
+				padding: "0",
+				paddingRight: "4em",
+				paddingLeft: "2em"
+			},
+			logoStyle: {
+				width: "2vw"
+			}
+		};
+	}
+	render() {
+		return (
+			<Fragment>
+				<div style={this.state.headerBG}>
+					<div style={this.state.headerFilter}>
+						<img
+							src={YellowDiamond}
+							style={this.state.logoStyle}
+							alt="Yellow Diamond"
+						/>
+						<h1>Hello Header</h1>
+					</div>
+				</div>
+			</Fragment>
+		);
+	}
+}
+export default Header;
